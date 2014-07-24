c6087bdc77a87d61764cff3f65bdc013614c6e4e
Sebastien Luong <sebastienluong@gmail.com>
2014-07-23 20:51:51 -0400

    Filters found mixes in terms of certification

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ Progress:
 
 * The 8tracks API is successfully queried for mixes and prints out the top 10 return queries
   for specific mix types
-	
+* Filtering queries in order to return specific certifications [gem/gold/platinum]
+* Returns queries for the top 10 pages in trending or popular	
 
 ================
 Next steps: 
-* Filtering queries in order to return specific certifications [gem/gold/platinum]
-* Return more than 10 queries
+* Allow user input for different tags and certifications
 * Display return queries on webpage 
 * Implement form to allow user input and search for tags 
--- git_gem_finder.py
+++ git_gem_finder.py
@@ -15,26 +15,23 @@ def show_index():
 	api = 'API_KEY'
 	api_url = 'https://8tracks.com/sets/new.json?api_key='+api+'?api_version=3'
 	#mix_url = "http://8tracks.com/mix_sets/all.json?include=mixes[likes_count+3]&api_key="+api
-	# How do you only show certain mixes passing the include qualifications??
-	mix_url = "http://8tracks.com/mix_sets/tags:chill+electronic.json?include=mixes[certification[gold]]&api_key="+api+'&api_version=3'
 	top_tag_url = "http://8tracks.com/tags.json?api_key="+api	
 	# genre = raw_input("Enter mood, genre or activity")
-	r = requests.get(mix_url)
-	r.text
-	json_result = json.loads(r.text)
-	#print(r.text)
-	for results in json_result['mix_set']['mixes']:
-		print "Mix name: "+results['name']
-		print "Likes count: "+str(results['likes_count'])
-		
-	#for results in json_result['mixes']:
-	#	print "Mix name: "+results['name']
-	'''
-	for results in json_result['tags']: 
-		print "Tag name: " + results['name']
-		print "Cool tagging count: "+ results['cool_taggings_count']
-		print "------------------"	
-	'''
+	# give choice to search through popular or trending
+	for i in range(1,10):
+		mix_url = "http://8tracks.com/mix_sets/tags:korean:trending.json?include=mixes[likes_count]&page="+str(i)+"&api_key="+api+'&api_version=3'
+		r = requests.get(mix_url)
+		r.text
+		json_result = json.loads(r.text)
+		#print(r.text)
+		for results in json_result['mix_set']['mixes']:
+			if (results['certification']=='gold'):
+				print "Mix name: "+results['name']
+				print "Likes count: "+str(results['likes_count'])
+				print "Certification: "+results['certification']
+				print "8tracks path: "+results['path']
+				print "Page found: "+str(i)
+	
 	if request.method == 'POST':
 		# get input request
 		result = request.form['queryBox']
